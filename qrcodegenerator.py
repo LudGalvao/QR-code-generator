c1d799290dbb09dcb61428a8cc53d0cb24201650
xlima <99023858+Xlima360@users.noreply.github.com>
2022-04-28 10:51:29 -0300

    Add files via upload

---
+++ qrcodegenerator.py
@@ -0,0 +1,14 @@
+import qrcode
+import image
+qr = qrcode.QRCode(
+    version = 15,
+    box_size = 10,
+    border = 5
+)
+
+data = #"https://twitter.com/Overvghoul"
+
+qr.add_data(data)
+qr.make(fit = True)
+img = qr.make_image(fill="black",back_color = "white")
+img.save("test.png")
